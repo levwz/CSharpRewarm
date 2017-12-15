702ff375894920b64fdc06d8eb4babc8e2d95188
Zhou, Wei <wei.zhou03@sap.com>
2017-12-15 17:58:18 +0800

    init the project

---
+++ CSharpStudies/CSharpStudies/LambdaDelegateAnonymous.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CSharpStudies
+{
+    public class LambdaDelegateAnonymous
+    {
+        public LambdaDelegateAnonymous()
+        {
+        }
+
+        public void Test(){
+            Console.WriteLine("LambdaDelegateAnonymous Methods test");
+        }
+    }
+}
---
+++ CSharpStudies/CSharpStudies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpStudies
+{
+    class MainClass
+    {
+        delegate void DelegateVoid();
+        public static void Main(string[] args)
+        {
+            LambdaDelegateAnonymous lda = new LambdaDelegateAnonymous();
+
+            DelegateVoid dv = new DelegateVoid(lda.Test);
+
+            dv();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
